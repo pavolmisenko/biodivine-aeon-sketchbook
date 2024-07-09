63e152d46a70ca5ac8a97a2eb374387421a1a88d
Pavol Mišenko <pavolmisenko.95@gmail.com>
2024-07-09 14:50:09 +0200

    pre-development changes to run sketchbook.

--- src/html/component/regulations-editor/regulations-import/regulations-import.ts
+++ src/html/component/regulations-editor/regulations-import/regulations-import.ts
@@ -1,33 +1,32 @@
-import { LitElement, TemplateResult, html, unsafeCSS } from "lit";
-import { customElement, state } from "lit/decorators.js";
+import { LitElement, type TemplateResult, html, unsafeCSS } from 'lit'
+import { customElement, state } from 'lit/decorators.js'
 
 // Include the content of regulations-import.less as raw string variable.
 import style_less from './regulations-import.less?inline'
 
 @customElement('regulations-import')
 export default class RegulationsImport extends LitElement {
+  // Informs lit that the CSS included for this component comes
+  // from the `style_less` variable (we could also write CSS
+  // right here using css`...` string literal).
+  static styles = unsafeCSS(style_less)
 
-    // Informs lit that the CSS included for this component comes 
-    // from the `style_less` variable (we could also write CSS
-    // right here using css`...` string literal).
-    static styles = unsafeCSS(style_less)
+  // Internal state of the component that is not visible to the
+  // "outside" world. Use @property to declare state that can be
+  // modified by external components.
+  @state() counter = 0
 
-    // Internal state of the component that is not visible to the
-    // "outside" world. Use @property to declare state that can be
-    // modified by external components. 
-    @state() counter = 0
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  increment () {
+    this.counter += 1
+  }
 
-    increment() {
-        this.counter += 1
-    }
-
-    protected render(): TemplateResult {
-        return html`
+  protected render (): TemplateResult {
+    return html`
             <div class="uk-margin-top uk-margin-bottom uk-margin-left uk-margin-right">
                 Count: <span class="uk-label">${this.counter}</span>
                 <button @click="${this.increment}" class="uk-button uk-button-primary">Add</button>
             </div>
-        `;
-    }
-    
+        `
+  }
 }
